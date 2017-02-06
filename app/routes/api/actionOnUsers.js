36b1316e41a78e99d98e98d0ef18b8abd40b3f1c
Мирослав Здравковић <miroslav.zdravkovic@gmail.com>
2017-02-06 21:42:35 +0100

    Update the user`s info using PUT request

--- app/routes/api/actionOnUsers.js
+++ app/routes/api/actionOnUsers.js
@@ -16,14 +16,37 @@ module.exports = function(app, express) {
         // get the user with that ID
         // (accessed at http://localhost:8080/api/users/:user_id)
         .get(function(req, res) {
-
-            console.log('KUKURIKU: ', req.params.user_id);
             
             User.findById(req.params.user_id, function(err, user) {
                 if (err) return res.send(err);
 
                 // return that user
                 res.json(user);
+            });
+        })
+
+        // update the user with this id
+        // (accessed at PUT http://localhost:8080/api/users/:user_id)
+        .put(function(req, res) {
+
+            // use our user model to find the user we want
+            User.findById(req.params.user_id, function(err, user) {
+
+                if (err) res.send(err);
+
+                // update the user info only if its new
+                if (req.body.name) user.name = req.body.name;
+                if (req.body.username) user.username = req.body.username;
+                if (req.body.password) user.password = req.body.password;
+
+                // save the user
+                user.save(function(err) {
+                    if (err) res.send(err);
+
+                    // return the message
+                    res.json({ message: 'User updated!'});
+                });
+
             });
         })
 
