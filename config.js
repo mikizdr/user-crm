19eb9fe80f4ea70552e175efd12e25ad60a838c3
Мирослав Здравковић <miroslav.zdravkovic@gmail.com>
2017-02-13 16:38:17 +0100

    Changed the name of the database from contactlist to crm_users

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'port' : process.env.PORT || 8080,
     //'database' : 'mongodb://user:kikiriki07@ds111798.mlab.com:11798/contact_list',
-    'database' : 'mongodb://localhost:27017/contactlist',
+    'database' : 'mongodb://localhost:27017/crm_users',
     'secret' : 'leabelka'
 };
